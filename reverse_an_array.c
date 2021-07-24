41074a419997dac1050137764a0bb2e249de17e0
Ujjwal <shanusharma588@gmail.com>
2021-07-24 20:29:29 +0530

    Added reverse_an_array.c

---
+++ reverse_an_array.c
@@ -0,0 +1,36 @@
+/* Program to reverse a given array
+ *
+ * Compilation: gcc -o reverse reverse_an_array.c
+ * Execution: ./reverse
+ *
+ * @Ujjwal, 1910991390, 24/07/2021
+ * Assignment 1 (C_Foundations)
+ */
+
+#include <stdio.h>
+
+void reverse(int *num) {
+	
+    printf("%d ",*num);
+}
+
+int main() {
+
+    int size;
+    int arr[size];
+
+    printf("Enter the size of Array:");
+    scanf("%d",&size);
+    
+    for(int i = 0; i < size; i++) {
+    
+        scanf("%d",&arr[i]);	    
+    }
+    for(int i = size-1; i >= 0; i--) {
+
+        reverse(&arr[i]);
+    
+    }
+    
+    return 0;
+}
