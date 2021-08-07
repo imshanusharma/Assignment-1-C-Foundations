e16b90dc904b27f80e01ddbd2d282478c6ab1ecd
Ujjwal <shanusharma588@gmail.com>
2021-08-07 18:00:14 +0530

    updated 7_file.c

--- 7_file.c
+++ 7_file.c
@@ -1,6 +1,6 @@
 /* Program to print lines of a file which are greater than 80 characters.
  *
- * Compilation: gcc -o file file.c
+ * Compilation: gcc -o file 7_file.c
  * Execution: ./file
  *
  * @Ujjwal, 1910991390, 07/08/2021
@@ -8,6 +8,8 @@
  */
 
 #include <stdio.h>
+
+/* Function to calculate the length of string*/
 
 int customstrlen(char* str) {
 
@@ -26,13 +28,19 @@ int main() {
     FILE* fp;
     char line[200];
 
+    /*Opening a file*/
+
     fp = fopen("7_a.txt", "r");
+
+    /*Checking the file is empty or not*/
 
     if(fp == NULL)
     {
 	printf("File is empty!");
 	return 0;
     }
+
+    /*fetching lines from file and printing which are greater than 80 characters*/
 
     while(fgets(line,200,fp))
     {
@@ -41,6 +49,8 @@ int main() {
 	    printf("%s", line);
 	}
     }
+
+    /*Closing the file*/
 
     fclose(fp);
     return 0;
